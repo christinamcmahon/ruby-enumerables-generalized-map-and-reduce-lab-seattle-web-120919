4b1519a18a7765b85b95a1e92c7cef5b404f4e45
Christina McMahon <christina.mcmahon1@gmail.com>
2019-12-02 20:24:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each do |item|
+    new_array.push(yield(item))
+  end
+  new_array
+end
+
+def reduce(source_array, start = 0)
+  result = start
+  source_array.each do |item|
+    result = yield(result, item)
+  end
+  result
+end
