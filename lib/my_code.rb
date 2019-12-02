24c2ba78beeb0f398da887fe3d219b92e604e9b2
Christina McMahon <christina.mcmahon1@gmail.com>
2019-12-02 20:39:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,7 @@ end
 def reduce(source_array, start = 0)
   result = start
   source_array.each do |item|
-    
     result = yield(result, item)
-    if item == true  
-      return true 
-    end
   end
   result
 end
